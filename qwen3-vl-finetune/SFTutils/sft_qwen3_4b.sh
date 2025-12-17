62c9f0cafc4af684cecb48b67a730192481cc1e0
Bryan Guan <cnboyuguan@gmail.com>
2025-12-17 19:19:30 +0800

    完善SFT脚本

--- qwen3-vl-finetune/SFTutils/sft_qwen3_4b.sh
+++ qwen3-vl-finetune/SFTutils/sft_qwen3_4b.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 export CUDA_VISIBLE_DEVICES=2,3
 
@@ -23,47 +23,49 @@ grad_accum_steps=4
 entry_file=train/train_qwen.py
 
 # Dataset configuration (replace with public dataset names)
-datasets=vmtsft
+datasets="vmtsft%40"
 
 # Output configuration
 run_name="qwen3-vl-4b-sft"
 output_dir=./output
 
 # Training arguments
-args="
-    --deepspeed ${deepspeed} \
-    --model_name_or_path "${llm}" \
-    --dataset_use ${datasets} \
-    --data_flatten True \
-    --tune_mm_vision False \
-    --tune_mm_mlp True \
-    --tune_mm_llm True \
-    --bf16 \
-    --output_dir ${output_dir} \
-    --num_train_epochs 0.5 \
-    --per_device_train_batch_size ${batch_size} \
-    --per_device_eval_batch_size $((batch_size*2)) \
-    --gradient_accumulation_steps ${grad_accum_steps} \
-    --max_pixels 50176 \
-    --min_pixels 784 \
-    --eval_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 1000 \
-    --save_total_limit 1 \
-    --learning_rate ${lr} \
-    --weight_decay 0 \
-    --warmup_ratio 0.03 \
-    --max_grad_norm 1 \
-    --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --model_max_length 8192 \
-    --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
-    --run_name ${run_name} \
-    --report_to wandb"
+args=(
+    --deepspeed "${deepspeed}"
+    --model_name_or_path "${llm}"
+    --dataset_use "${datasets}"
+    --data_flatten True
+    --tune_mm_vision False
+    --tune_mm_mlp True
+    --tune_mm_llm True
+    --bf16
+    --output_dir "${output_dir}"
+    --num_train_epochs 2
+    --per_device_train_batch_size "${batch_size}"
+    --per_device_eval_batch_size $((batch_size*2))
+    --gradient_accumulation_steps "${grad_accum_steps}"
+    --max_pixels 401408
+    --min_pixels 3136
+    --eval_strategy "no"
+    --save_strategy "steps"
+    --save_steps 200
+    --save_total_limit 20
+    --learning_rate "${lr}"
+    --weight_decay 0
+    --warmup_ratio 0.03
+    --max_grad_norm 1
+    --lr_scheduler_type "cosine"
+    --logging_steps 1
+    --model_max_length 16384
+    --dataloader_num_workers 16
+    --dataloader_prefetch_factor 4
+    --run_name "${run_name}"
+    --report_to wandb
+)
+    # --gradient_checkpointing True \
 
 # Launch training
 torchrun --nproc_per_node=${NPROC_PER_NODE} \
-         --master_addr=${MASTER_ADDR} \
-         --master_port=${MASTER_PORT} \
-         ${entry_file} ${args}
+            --master_addr=${MASTER_ADDR} \
+            --master_port=${MASTER_PORT} \
+            ${entry_file} "${args[@]}"
